1ac946efd476b3c534ab1c6a1129d4c6b5ecb00c
Phyma <setra856@student.liu.se>
2017-09-07 23:27:10 +0200

    Fixed so it auto take host and port from get message. Try and close after one iteratione, dosent go so well.

--- Lab2/Helper.py
+++ Lab2/Helper.py
@@ -12,26 +12,42 @@ class NetNinny:
     def poll(self):
         try:
             while True:
+                #Set default port in case no other is declared
+                port = 80
+                print("Before accept")
                 (clientSocket, client) = self.clientServerSocket.accept()
                 clientData = clientSocket.recv(1024)
-
+                print(clientData)
                 self.proxyToServer.settimeout(15)
 
-                self.proxyToServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-                self.proxyToServer.connect(('www.ida.liu.se', 80))
+                print(clientData.decode("utf-8"))
 
-                print(self.proxyToServer.send(clientData))
+                #Get host and port from GET message
+                splittedLines = clientData.decode("utf-8").splitlines();
+
+                #Loop thorugh all objects and take Host
+                for elem in splittedLines:
+                    if "Host" in elem:
+                        host = splittedLines[1].split(" ")[1]
+                        break
+
+                #Check if host contains port
+                if ":" in host:
+                    splitstring = host.split(":")
+                    host = splitstring[0]
+                    port = splitstring[1]
+
+                print("Host: ", host, "Port: ", port)
+                self.proxyToServer.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                self.proxyToServer.connect((host, int(port)))
+
+                self.proxyToServer.send(clientData)
 
                 recieved = self.proxyToServer.recv(1024)
-
-                print(recieved)
-
                 clientSocket.send(recieved)
 
                 self.proxyToServer.close()
-                self.clientServerSocket.close()
-
-                break
+                #clientSocket.close()
 
         except socket.error as msg:
             print(msg)
