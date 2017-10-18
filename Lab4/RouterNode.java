61048e4fedca2afc1b35a696c912fd354248eab1
Phyma <sethramstrom@gmail.com>
2017-10-18 23:40:44 +0200

    Implemented double int to save distance from other nodes

--- Lab4/RouterNode.java
+++ Lab4/RouterNode.java
@@ -6,8 +6,8 @@ public class RouterNode {
   private GuiTextArea myGUI;
   private RouterSimulator sim;
   private int[] costs = new int[RouterSimulator.NUM_NODES];
-  private int[] distance = new int[RouterSimulator.NUM_NODES];
-  private Integer[] routes = new Integer[RouterSimulator.NUM_NODES];
+  private int[][] distance = new int[RouterSimulator.NUM_NODES][RouterSimulator.NUM_NODES];
+  private int[] routes = new int[RouterSimulator.NUM_NODES];
 
   private boolean distance_change = false;
   private boolean poison_enabled = false;
@@ -20,54 +20,34 @@ public class RouterNode {
 
     System.arraycopy(costs, 0, this.costs, 0, RouterSimulator.NUM_NODES);
 
-    for (int i = 0; i < this.sim.NUM_NODES; i++){
-      distance[i] = costs[i];
+    for (int i = 0; i < RouterSimulator.NUM_NODES; i++){
+        if(i == myID){
+            distance[i] = costs;
+        } else{
+            for(int j = 0; j < RouterSimulator.NUM_NODES; j++){
+               distance[i][j] = RouterSimulator.INFINITY;
+            }
+        }
     }
 
-    for (int i = 0; i < this.sim.NUM_NODES; i++){
-      if (costs[i] != this.sim.INFINITY){
+    for (int i = 0; i < RouterSimulator.NUM_NODES; i++){
+      if (costs[i] != RouterSimulator.INFINITY){
         routes[i] = i;
-        sendUpdate(new RouterPacket(myID, i, distance));
       } else {
-        routes[i] = null;
+        routes[i] = RouterSimulator.INFINITY;
       }
     }
-
+    updateNeighbor(distance[myID]);
     printDistanceTable();
   }
 
   //--------------------------------------------------
+  //int sourceid, int destid, int[] mincost
   public void recvUpdate(RouterPacket pkt) {
-    distance_change = false;
+      distance[pkt.sourceid] = pkt.mincost;
 
-    for (int i = 0; i < this.sim.NUM_NODES; i++){
-      if (i != myID && i != pkt.sourceid){
-        if (distance[i] > pkt.mincost[i] + costs[pkt.sourceid]) {
-          distance[i] = pkt.mincost[i] + costs[pkt.sourceid];
-          routes[i] = routes[pkt.sourceid];
+      updateCosts(false);
 
-          distance_change = true;
-        }
-        if ((routes[i] == pkt.sourceid) && (costs[i] < (costs[pkt.sourceid] + pkt.mincost[i]))){
-          distance[i] = costs[i];
-          routes[i] = i;
-
-          distance_change = true;
-        }
-
-        if (!distance_change && pkt.mincost[i] > distance[i] + distance[pkt.sourceid]){
-          sendUpdate(new RouterPacket(myID, i, distance));
-        }
-      }
-    }
-
-    if (distance_change) {
-      for (int i = 0; i < this.sim.NUM_NODES; i++) {
-        if (costs[i] != this.sim.INFINITY) {
-          sendUpdate(new RouterPacket(myID, i, distance));
-        }
-      }
-    }
   }
 
   //--------------------------------------------------
@@ -101,7 +81,7 @@ public class RouterNode {
     myGUI.println();
     myGUI.print("D: ");
     for(int i = 0; i < distance.length; i++){
-      myGUI.print(distance[i] + " ");
+      myGUI.print(distance[myID][i] + " ");
     }
 
     myGUI.println("\n");
@@ -109,11 +89,21 @@ public class RouterNode {
 
   //--------------------------------------------------
   public void updateLinkCost(int dest, int newcost) {
-    distance_change = false;
+      System.out.println("1 MyID: " + myID + " Distance: " + Arrays.toString(distance[myID]));
+      costs[dest] = newcost;
+     /* for (int i = 0; i < RouterSimulator.NUM_NODES; i++) {
+          if (routes[i] == dest){
+              distance[myID][i] = newcost;
+          }
+      }*/
+      System.out.println("2 MyID: " + myID + " Distance: " + Arrays.toString(distance[myID]));
+      updateCosts(true);
+    // distance_change = false;
 
-    costs[dest] = newcost;
+   /* costs[dest] = newcost;
 
-    if (costs[dest] < distance[dest]) {
+
+      if (costs[dest] < distance[dest]) {
       distance[dest] = costs[dest];
       routes[dest] = dest;
 
@@ -125,12 +115,47 @@ public class RouterNode {
     }
 
     if (distance_change) {
-      for (int i = 0; i < this.sim.NUM_NODES; i++) {
-        if (costs[i] != this.sim.INFINITY) {
-          sendUpdate(new RouterPacket(myID, i, distance));
-        }
+        updateNeighbor(distance[myID]);
+    }*/
+  }
+
+  public void updateNeighbor(int[] distance){
+      for (int i = 0; i < RouterSimulator.NUM_NODES; i++) {
+          if (costs[i] != RouterSimulator.INFINITY && i != myID) {
+              sendUpdate(new RouterPacket(myID, i, distance));
+          }
       }
-    }
+  }
+
+  public void updateCosts(boolean link){
+      distance_change = false;
+      for(int dest = 0; dest < RouterSimulator.NUM_NODES; dest++){
+          if(dest != myID){
+              for(int j = 0; j < RouterSimulator.NUM_NODES; j++){
+                  if(costs[j] != RouterSimulator.INFINITY){
+                      if(distance[myID][dest] > distance[j][dest] + costs[j]){
+                          if(link)
+                              System.out.println("ID: " + myID+ " j: " + j + " dest: " + dest + " myID distance: " + distance[myID][dest] + " costs: " + costs[j] + " distance J: " + distance[j][dest]+ " route: " + routes[dest]);
+                          distance[myID][dest] = distance[j][dest] + costs[j];
+                          routes[dest] = j;
+                          distance_change = true;
+                      } else if(routes[j] == dest){
+
+                      }
+                  }
+              }
+          }
+      }
+
+      if(!distance_change){
+          System.out.println("Not changed");
+         //System.out.println("ID: " + myID+ " costs: " + Arrays.toString(costs) + " distance: " + Arrays.toString(distance[myID]));
+      }
+
+      if (distance_change) {
+          System.out.println("UPDATED");
+          updateNeighbor(distance[myID]);
+      }
   }
 }
 
